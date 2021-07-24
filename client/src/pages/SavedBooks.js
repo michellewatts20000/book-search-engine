46f6519ffd994c3e0ecc62d53265802208a2c535
Your NameMichelle <watts.e.michelle@gmail.com>
2021-07-24 14:23:04 +1000

    mutations

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -4,7 +4,7 @@ import { REMOVE_BOOK } from '../utils/mutations';
 import { GET_ME } from '../utils/queries';
 
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-// import { getMe, deleteBook } from '../utils/API';
+
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,15 +1,27 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-mutation loginUser($email: String!, $password: String!) {
-  login(email: $email, password: $password) {
-    token
-    user {
-      _id
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          image
+          description
+          title
+          link
+        }
+      }
     }
   }
-}
 `;
+
 
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
